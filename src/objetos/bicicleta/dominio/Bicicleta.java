d7bbd745b3d0c4571ead790f1263b4d5ed3169a3
venilson <venilsoncosta21@gmail.com>
2024-01-12 08:01:59 -0300

    criando o objeto bicicleta

--- src/objetos/bicicleta/dominio/Bicicleta.java
+++ src/objetos/bicicleta/dominio/Bicicleta.java
@@ -1,4 +1,68 @@
 package objetos.bicicleta.dominio;
 
+
 public class Bicicleta {
+    private String marca;
+    private String modelo;
+    private String cor;
+    private double velocidade = 0.0;
+
+    public Bicicleta() {
+    }
+
+    public Bicicleta(String marca, String modelo, String cor) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+    }
+
+    public void pedalar(double aceleracao){
+        if(getVelocidade() >= 0){
+            velocidade += aceleracao;
+        }
+    }
+
+    public double pegarVelocidadeAtual(){
+        return getVelocidade();
+    }
+
+    @Override
+    public String toString() {
+        return "MARCA: " + marca.toUpperCase() +
+                ", MODELO: " + modelo.toUpperCase() +
+                ", COR: " + cor.toUpperCase();
+    }
+
+    //getters e setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
 }
---
+++ src/objetos/bicicleta/teste/BicicletaTeste.java
@@ -0,0 +1,12 @@
+package objetos.bicicleta.teste;
+
+import objetos.bicicleta.dominio.Bicicleta;
+
+public class BicicletaTeste {
+    public static void main(String[] args) {
+        Bicicleta bicicleta1 = new Bicicleta("Caloi", "Híbrida", "Preta");
+        System.out.println(bicicleta1);
+        bicicleta1.pedalar(10.2);
+        System.out.println(bicicleta1.pegarVelocidadeAtual());
+    }
+}
